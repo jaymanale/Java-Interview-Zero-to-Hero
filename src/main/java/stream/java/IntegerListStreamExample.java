9b4ce85314cd5ec44c87951901ce16403a3a01eb
jaymanale <jaymanale29@gmail.com>
2022-08-24 19:27:34 +0530

    Minor Improvement

--- src/main/java/stream/java/IntegerListStreamExample.java
+++ src/main/java/stream/java/IntegerListStreamExample.java
@@ -10,7 +10,6 @@ public class IntegerListStreamExample {
 // DATA
         List<Integer> INTEGER_LIST = PopulateListData.getIntegerList(); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-
 //        Example: 1 -> Find even numbers from given ArrayList
 //        Expected Output : [2, 4, 6, 8, 10]
 
@@ -18,16 +17,18 @@ public class IntegerListStreamExample {
                 .stream()
                 .filter(number -> number % 2 == 0)
                 .toList();
-        System.out.println("Even Number -> " + evenNumberList);
+        System.out.println("Even Number -> " + evenNumberList); // Even Number -> [2, 4, 6, 8, 10]
 
 
-//        Example: 1 -> Find odd numbers from given ArrayList
+//        Example: 2 -> Find odd numbers from given ArrayList
 //        Expected Output : [1, 3, 5, 7, 9]
 
         List<Integer> oddNumberList = INTEGER_LIST
                 .stream()
                 .filter(number -> number % 2 != 0)
                 .toList();
-        System.out.println("Odd Number -> " + oddNumberList);
+        System.out.println("Odd Number -> " + oddNumberList); // Odd Number -> [1, 3, 5, 7, 9]
+
+
     }
 }
