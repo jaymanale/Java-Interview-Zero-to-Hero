c1c36138c56b7d8d9cf6fd0a6010c4a540275837
jaymanale <jaymanale29@gmail.com>
2022-08-25 10:06:51 +0530

    Duplicate and Unique number from list example add

--- src/main/java/stream/java/IntegerListStreamExample.java
+++ src/main/java/stream/java/IntegerListStreamExample.java
@@ -2,13 +2,17 @@ package stream.java;
 
 import util.PopulateListData;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class IntegerListStreamExample {
     public static void main(String[] args) {
 
 // DATA
         List<Integer> INTEGER_LIST = PopulateListData.getIntegerList(); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        List<Integer> DUPLICATE_INTEGER_LIST = PopulateListData.getDuplicateIntegerList(); // 1, 2, 2, 3, 3, 4, 5, 6, 6, 7, 8, 8
 
 //        Example: 1 -> Find even numbers from given ArrayList
 //        Expected Output : [2, 4, 6, 8, 10]
@@ -29,6 +33,23 @@ public class IntegerListStreamExample {
                 .toList();
         System.out.println("Odd Number -> " + oddNumberList); // Odd Number -> [1, 3, 5, 7, 9]
 
+//        Example: 3 Find duplicate number from given ArrayList
+//        Expected Output : [2, 3, 6, 8]
 
+        Set<Integer> duplicateSet = new HashSet<>();
+        List<Integer> duplicateNumber = DUPLICATE_INTEGER_LIST
+                .stream()
+                .filter(number -> !duplicateSet.add(number))
+                .toList();
+        System.out.println("Duplicate number -> " + duplicateNumber); // [2, 3, 6, 8]
+
+//        Example: 4 Find unique number from given ArrayList
+//        Expected Output : [1, 2, 3, 4, 5, 6, 7, 8]
+        Set<Integer> uniqueSet = new HashSet<>();
+        List<Integer> uniqueNumber = DUPLICATE_INTEGER_LIST
+                .stream()
+                .filter(uniqueSet::add)
+                .toList();
+        System.out.println("Unique number -> " + uniqueNumber); // [1, 2, 3, 4, 5, 6, 7, 8]
     }
 }
--- src/main/java/util/PopulateListData.java
+++ src/main/java/util/PopulateListData.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 public class PopulateListData {
 
-    public static List<Integer> getIntegerList(){
+    public static List<Integer> getIntegerList() {
         return List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     }
+
+    public static List<Integer> getDuplicateIntegerList() {
+        return List.of(1, 2, 2, 3, 3, 4, 5, 6, 6, 7, 8, 8);
+    }
+
 }
